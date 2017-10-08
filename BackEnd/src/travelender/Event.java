45331eed78775c9ac12ca1e14e1360bd152be7c4
DESKTOP-DCPGDTE\Dewi Roaza <dewi.roaza.tif16@polban.ac.id>
2017-10-08 20:27:54 +0700

    Nambah class provinsi dan kota

--- BackEnd/src/travelender/Event.java
+++ BackEnd/src/travelender/Event.java
@@ -15,6 +15,16 @@ import java.util.List;
  */
 public class Event {
 
+    
+    private String idUser;
+    private String idEvent;
+    private Date startTime;
+    private Date endTime;
+    private String nameEvent;
+    private ModaTransportasi transport;
+    private Lokasi destination;
+    private Lokasi embarkation;
+    
     /**
      * @return the idEvent
      */
@@ -42,14 +52,6 @@ public class Event {
     public void setNameEvent(String nameEvent) {
         this.nameEvent = nameEvent;
     }
-    private String idUser;
-    private String idEvent;
-    private Date startTime;
-    private Date endTime;
-    private String nameEvent;
-    private ModaTransportasi transport;
-    private Lokasi destination;
-    private Lokasi embarkation;
     /**
      * @return the idUser
      */
