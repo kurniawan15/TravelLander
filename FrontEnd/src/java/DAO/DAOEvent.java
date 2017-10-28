d0819dc93639a9d757859fd457ecb787c3dda152
MOHRIDWAN-HDP\Lenovo <mohridwan.hdp@gmail.com>
2017-10-29 05:06:40 +0700

    Progress EventDAO

--- FrontEnd/src/java/DAO/DAOEvent.java
+++ FrontEnd/src/java/DAO/DAOEvent.java
@@ -10,6 +10,7 @@ import Database.KoneksiDB;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -22,7 +23,7 @@ import model.Lokasi;
  */
 public class DAOEvent extends Event implements DAO{
     
-    KoneksiDB db = null;
+    static KoneksiDB db = null;
     
     public DAOEvent() {
        db = new KoneksiDB();
@@ -93,5 +94,74 @@ public class DAOEvent extends Event implements DAO{
             System.out.println("Terjadi Kesalah Saat menampilkan Cari ID" + ex);
         }
         return ev;
+    }
+    
+    public String getNewId() throws SQLException{
+            
+            //EV230917TR000101
+            SimpleDateFormat format = new SimpleDateFormat("ddMMyy");       
+            String idEvent = "EV" + format.format(new Date());
+            String kdTraveller = "TR0001";
+            
+            idEvent+=kdTraveller;
+            
+            int cnt = 0;
+            String sql = "SELECT MAX(id_event) FROM event WHERE substring(id_event) = '"+idEvent+"'"; 
+            
+            ResultSet resultSet = db.ambilData(sql);
+
+            while (resultSet.next()) {
+                idEvent = resultSet.getString(1);
+            } 
+            
+            cnt = Integer.parseInt(idEvent.substring(3));
+            cnt++;
+            if(cnt < 10){
+                idEvent = "EV" + kdTraveller + "0" +String.valueOf(cnt);
+            }
+            else{
+               idEvent = "EV" + kdTraveller +String.valueOf(cnt);
+            }
+            db.diskonek(resultSet);
+            return idEvent;
+         }
+    
+
+    public static void main(String[]args){
+        SimpleDateFormat format = new SimpleDateFormat("ddMMyy");       
+            String idEvent = "EV" + format.format(new Date());
+            String kdTraveller = "TR0001";
+            
+            idEvent+=kdTraveller;
+            try{
+            int cnt = 0;
+            String sql = "SELECT MAX(id_event) FROM event WHERE substring(id_event,1,14) = '"+idEvent+"'"; 
+            System.out.println(idEvent);
+            ResultSet resultSet = new DAOEvent().db.ambilData(sql);
+
+            while (resultSet.next()) {
+                idEvent = resultSet.getString(1);
+                System.out.println(resultSet.getString(1));
+            } 
+            
+            System.out.println(idEvent);
+            cnt = Integer.parseInt(idEvent.substring(14));
+            cnt++;
+            
+            if(cnt < 10){
+                idEvent = "EV" + kdTraveller + "0" +String.valueOf(cnt);
+            }
+            else{
+               idEvent = "EV" + kdTraveller +String.valueOf(cnt);
+            }
+            
+            db.diskonek(resultSet);
+            
+            System.out.println(idEvent);
+    }
+            
+            catch(Exception ex){
+                
+            }
     }
 }
