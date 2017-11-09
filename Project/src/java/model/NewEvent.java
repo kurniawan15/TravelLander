7d6b43e20a3ebeacb55a916b9ccd54d93bb98a21
DESKTOP-DCPGDTE\Dewi Roaza <dewi.roaza.tif16@polban.ac.id>
2017-11-09 19:11:50 +0700

    new class event dan new class lokasi

---
+++ Project/src/java/model/NewEvent.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Dewi Roaza
+ */
+public class NewEvent {
+    private String kdEvent;
+    private String namaEvent;
+    private Date waktuMulai;
+    private Date waktuSelesai;
+    private String ket;
+
+    /**
+     * @return the kdEvent
+     */
+    public String getKdEvent() {
+        return kdEvent;
+    }
+
+    /**
+     * @param kdEvent the kdEvent to set
+     */
+    public void setKdEvent(String kdEvent) {
+        this.kdEvent = kdEvent;
+    }
+
+    /**
+     * @return the namaEvent
+     */
+    public String getNamaEvent() {
+        return namaEvent;
+    }
+
+    /**
+     * @param namaEvent the namaEvent to set
+     */
+    public void setNamaEvent(String namaEvent) {
+        this.namaEvent = namaEvent;
+    }
+
+    /**
+     * @return the waktuMulai
+     */
+    public Date getWaktuMulai() {
+        return waktuMulai;
+    }
+
+    /**
+     * @param waktuMulai the waktuMulai to set
+     */
+    public void setWaktuMulai(Date waktuMulai) {
+        this.waktuMulai = waktuMulai;
+    }
+
+    /**
+     * @return the waktuSelesai
+     */
+    public Date getWaktuSelesai() {
+        return waktuSelesai;
+    }
+
+    /**
+     * @param waktuSelesai the waktuSelesai to set
+     */
+    public void setWaktuSelesai(Date waktuSelesai) {
+        this.waktuSelesai = waktuSelesai;
+    }
+
+    /**
+     * @return the ket
+     */
+    public String getKet() {
+        return ket;
+    }
+
+    /**
+     * @param ket the ket to set
+     */
+    public void setKet(String ket) {
+        this.ket = ket;
+    }
+    
+}
---
+++ Project/src/java/model/NewLokasi.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Dewi Roaza
+ */
+public class NewLokasi {
+    private String idLokasi;
+    private String namaLokasi;
+    private String latitude;
+    private String longitute;
+
+    /**
+     * @return the idLokasi
+     */
+    public String getIdLokasi() {
+        return idLokasi;
+    }
+
+    /**
+     * @param idLokasi the idLokasi to set
+     */
+    public void setIdLokasi(String idLokasi) {
+        this.idLokasi = idLokasi;
+    }
+
+    /**
+     * @return the namaLokasi
+     */
+    public String getNamaLokasi() {
+        return namaLokasi;
+    }
+
+    /**
+     * @param namaLokasi the namaLokasi to set
+     */
+    public void setNamaLokasi(String namaLokasi) {
+        this.namaLokasi = namaLokasi;
+    }
+
+    /**
+     * @return the latitude
+     */
+    public String getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * @param latitude the latitude to set
+     */
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * @return the longitute
+     */
+    public String getLongitute() {
+        return longitute;
+    }
+
+    /**
+     * @param longitute the longitute to set
+     */
+    public void setLongitute(String longitute) {
+        this.longitute = longitute;
+    }
+}
