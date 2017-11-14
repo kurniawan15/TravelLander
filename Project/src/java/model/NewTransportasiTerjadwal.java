c45b202224fd39fb74c3d2acfd36aba2f4c56bad
DESKTOP-DCPGDTE\Dewi Roaza <dewi.roaza.tif16@polban.ac.id>
2017-11-14 14:17:51 +0700

    Sudah Balance dengan database, untuk database sekarang ada 6 entity yaitu : 1. Event 2. Lokasi 3. Traveler 4. Transportasi terjadwal 5. Transportasi tidak terjadwal 6. Moda transportasi Untuk model di netbeans yang dibuat dari awal ada event, lokasi, transportasi terjadwal dan transportasi tidak terjadwal dengan tanda di awal ada New, untuk moda transportasi dan traveler masih mengunakan yang lama.

---
+++ Project/src/java/model/NewTransportasiTerjadwal.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Dewi Roaza
+ */
+public class NewTransportasiTerjadwal {
+    private String kdTransportasiTerjadwal;
+    private String namaTransportasiTerjadwal;
+    private Date waktuBerangkat;
+    private Date waktuDatang;
+
+    /**
+     * @return the kdTransportasiTerjadwal
+     */
+    public String getKdTransportasiTerjadwal() {
+        return kdTransportasiTerjadwal;
+    }
+
+    /**
+     * @param kdTransportasiTerjadwal the kdTransportasiTerjadwal to set
+     */
+    public void setKdTransportasiTerjadwal(String kdTransportasiTerjadwal) {
+        this.kdTransportasiTerjadwal = kdTransportasiTerjadwal;
+    }
+
+    /**
+     * @return the namaTransportasiTerjadwal
+     */
+    public String getNamaTransportasiTerjadwal() {
+        return namaTransportasiTerjadwal;
+    }
+
+    /**
+     * @param namaTransportasiTerjadwal the namaTransportasiTerjadwal to set
+     */
+    public void setNamaTransportasiTerjadwal(String namaTransportasiTerjadwal) {
+        this.namaTransportasiTerjadwal = namaTransportasiTerjadwal;
+    }
+
+    /**
+     * @return the waktuBerangkat
+     */
+    public Date getWaktuBerangkat() {
+        return waktuBerangkat;
+    }
+
+    /**
+     * @param waktuBerangkat the waktuBerangkat to set
+     */
+    public void setWaktuBerangkat(Date waktuBerangkat) {
+        this.waktuBerangkat = waktuBerangkat;
+    }
+
+    /**
+     * @return the waktuDatang
+     */
+    public Date getWaktuDatang() {
+        return waktuDatang;
+    }
+
+    /**
+     * @param waktuDatang the waktuDatang to set
+     */
+    public void setWaktuDatang(Date waktuDatang) {
+        this.waktuDatang = waktuDatang;
+    }
+}
---
+++ Project/src/java/model/NewTransportasiTidakTerjadwal.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Dewi Roaza
+ */
+public class NewTransportasiTidakTerjadwal {
+    private String kdTransportasiTidakTerjadwal;
+    private String namaTransportasiTidakTerjadwal;
+
+    /**
+     * @return the kdTransportasiTidakTerjadwal
+     */
+    public String getKdTransportasiTidakTerjadwal() {
+        return kdTransportasiTidakTerjadwal;
+    }
+
+    /**
+     * @param kdTransportasiTidakTerjadwal the kdTransportasiTidakTerjadwal to set
+     */
+    public void setKdTransportasiTidakTerjadwal(String kdTransportasiTidakTerjadwal) {
+        this.kdTransportasiTidakTerjadwal = kdTransportasiTidakTerjadwal;
+    }
+
+    /**
+     * @return the namaTransportasiTidakTerjadwal
+     */
+    public String getNamaTransportasiTidakTerjadwal() {
+        return namaTransportasiTidakTerjadwal;
+    }
+
+    /**
+     * @param namaTransportasiTidakTerjadwal the namaTransportasiTidakTerjadwal to set
+     */
+    public void setNamaTransportasiTidakTerjadwal(String namaTransportasiTidakTerjadwal) {
+        this.namaTransportasiTidakTerjadwal = namaTransportasiTidakTerjadwal;
+    }
+}
