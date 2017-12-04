780435e2a6c05601a8250ee2fcd4c3252d226b57
DESKTOP-GT5MRGL\Fadhil-PC <fadhil.shofian.tif16@polban.ac.id>
2017-12-04 23:28:11 +0700

    mengambil data dari google API dan di parse dari json ke java

---
+++ Project/src/java/Json/Distance.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Json;
+
+/**
+ *
+ * @author Fadhil-PC
+ */
+public class Distance {
+    private String text;
+    private Integer value;
+
+    /**
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
+
+    /**
+     * @param text the text to set
+     */
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    /**
+     * @return the value
+     */
+    public Integer getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
