3e95f25491c6214f118eb15ddd7380f996a9152a
DESKTOP-DCPGDTE\Dewi Roaza <dewi.roaza.tif16@polban.ac.id>
2017-12-17 18:19:08 +0700

    membuat class moda transit dan dao moda transit

---
+++ Project/src/java/model/NewModaTransit.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Dewi Roaza
+ */
+public class NewModaTransit {
+    public String kdEvent;
+    public String vehicleMode;
+    public String transit;
+
+    /**
+     * @return the kdEvent
+     */
+    public String getKdEvent() {
+        return kdEvent;
+    }
+
+    /**
+     * @param kdEvent the kdEvent to set
+     */
+    public void setKdEvent(String kdEvent) {
+        this.kdEvent = kdEvent;
+    }
+
+    /**
+     * @return the vehicleMode
+     */
+    public String getVehicleMode() {
+        return vehicleMode;
+    }
+
+    /**
+     * @param vehicleMode the vehicleMode to set
+     */
+    public void setVehicleMode(String vehicleMode) {
+        this.vehicleMode = vehicleMode;
+    }
+
+    /**
+     * @return the transit
+     */
+    public String getTransit() {
+        return transit;
+    }
+
+    /**
+     * @param transit the transit to set
+     */
+    public void setTransit(String transit) {
+        this.transit = transit;
+    }
+}
